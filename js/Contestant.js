904438d30e5509a753d6728267c946ecc6907daa
Alice 3 <101829188+Alice1Kamui@users.noreply.github.com>
2022-07-14 01:28:17 -0300

    Update Contestant.js

--- js/Contestant.js
+++ js/Contestant.js
@@ -9,7 +9,7 @@ class Contestant {
     var contestantCountRef = database.ref('contestantCount');
 
 
-    contestantCountRef.on("value",(data)=>{
+    contestantCountRef.on("value",function(data){
       contestantCount = data.val();
     })
 
